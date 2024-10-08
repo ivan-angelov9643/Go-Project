a9048f999d47a8fc1e35aad8128c65227593da95
Ivan Angelov <ivan.angelov03@sap.com>
2024-10-08 09:22:58 +0300

    Test handlers negavite outcome

--- handlers/object_handler.go
+++ handlers/object_handler.go
@@ -7,6 +7,10 @@ type ObjectHandler struct {
 }
 
 func (handler *ObjectHandler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+	if len(handler.Text) == 0 {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 	_, err := w.Write([]byte(handler.Text))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
--- handlers/object_handler_test.go
+++ handlers/object_handler_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestObjectHandler1(t *testing.T) {
+func TestObjectHandlerOK1(t *testing.T) {
 	req, err := http.NewRequest("GET", "/api/object1", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -29,7 +29,7 @@ func TestObjectHandler1(t *testing.T) {
 	}
 }
 
-func TestObjectHandler2(t *testing.T) {
+func TestObjectHandlerOK2(t *testing.T) {
 	req, err := http.NewRequest("GET", "/api/object2", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -48,5 +48,37 @@ func TestObjectHandler2(t *testing.T) {
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
+	}
+}
+
+func TestObjectHandlerEmptyString1(t *testing.T) {
+	req, err := http.NewRequest("GET", "/api/object1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := &handlers.ObjectHandler{Text: ""}
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusInternalServerError {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusInternalServerError)
+	}
+}
+
+func TestObjectHandlerEmptyString2(t *testing.T) {
+	req, err := http.NewRequest("GET", "/api/object2", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := &handlers.ObjectHandler{Text: ""}
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusInternalServerError {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusInternalServerError)
 	}
 }
